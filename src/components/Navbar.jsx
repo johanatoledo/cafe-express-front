abbd5fc349f6633dd7ab03b4709d004014dbec6c
johanatoledo <johanatoledo1990@gmail.com>
2026-05-14 12:41:37 -0500

    cambio en logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-30   bg-white/90 backdrop-blur-md">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2">
         <Link href="/" >
           <Image
              src="/branding/logo.png"
              alt="Café Express"
-             width={100}
-             height={100}
+             width={150}
+             height={150}
              className="object-contain"
           />
         </Link>
